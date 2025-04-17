23ecc73d1a9b66019742a6fc9e04e497d6a84fc0
Yi Daniel Chen Liu <danielyi2004@gmail.com>
2025-04-17 10:38:44 +0200

    Wezterm windows conf

---
+++ wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm';
+
+return {
+  default_prog = { "pwsh.exe" },
+  color_scheme = "Catppuccin Macchiato", -- or Macchiato, Frappe, Latte
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
+}
+
